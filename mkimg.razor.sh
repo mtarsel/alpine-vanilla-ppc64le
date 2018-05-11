1c74b2acd5f8e3359ee213878784a1f1f7d17a1c
root <root@alpineski.my.domain>
2018-05-11 15:07:34 -0700

    removed xtables addon

--- mkimg.razor.sh
+++ mkimg.razor.sh
@@ -7,7 +7,6 @@ profile_razor() {
 	arch="x86 x86_64 ppc64le"
 	output_format="iso"
 	kernel_cmdline="nomodeset"
-	kernel_addons="xtables-addons"
 	apks="$apks
 		bkeymaps alpine-base alpine-mirrors network-extras openssl openssh
 		chrony tzdata ruby ruby-rake ruby-bundler net-tools
